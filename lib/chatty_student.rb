c32535d3063c3f3477827029a23442518004cdfe
Tyler Firth <tyfirth@gmail.com>
2020-06-10 03:20:29 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,7 +6,16 @@ class ChattyStudent < Student
   end
   
   def raise_hand
-    super * 10
+    super 
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
   end
   
 end
